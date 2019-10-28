e2a35ee54b77ec38dc0c935c6f657bc170c5a1d0
Ethan Dunne <ebd007@bucknell.edu>
2019-10-28 11:37:25 -0400

    Initial Commit

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: Ethan Dunne
+ * Section: 11am
+ * Date: 10/28/19
+ * Time: 11:29 AM
+ *
+ * Project: csci205_hw02
+ * Package: main
+ * Class: Main
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HEFBHKS");
+    }
+}
